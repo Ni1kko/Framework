6334550fe28e9188eae6a8a5fc2490bdee3b293c
Nikko Renolds <Ni1kko@outlook.com>
2022-09-10 16:35:47 +0100

    Load order

--- framework/@server/addons/life_backend/scripts/WantedSystem/config.cpp
+++ framework/@server/addons/life_backend/scripts/WantedSystem/config.cpp
@@ -4,7 +4,7 @@ class CfgPatches
     {
         units[] = {};
         weapons[] = {};
-        requiredAddons[] = {};
+        requiredAddons[] = {"Systems"};
         authors[] = {"Ni1kko"};
     };
 };
